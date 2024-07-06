65cfd4f280e11d27a9c79e3bbe9b341d4b63a55b
James Herd <james.o.herd@gmail.com>
2024-07-06 23:09:51 +1000

    added functionality to temporarily disable cases while the bank offer is in place.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let playersCaseNumber;
 let playersCaseDollarValue;
+let openedCases = 0;
 let casesAndTheirDollarValues = [];
 let gameInstruction = document.getElementById("gameInstruction");
 
 // game initialization
-// assign game dollar values randomly to all cases
 (function assignDollarValuesToCases() {
   let caseNumbers = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
@@ -31,20 +31,66 @@ let gameInstruction = document.getElementById("gameInstruction");
 })();
 
 function selectedCase(caseNumber) {
-  // if player has selected a case
+  openedCases++;
+
+  for (let i = 0; i <= 25; i++) {
+    if (casesAndTheirDollarValues[i][0] === caseNumber) {
+      casesAndTheirDollarValues[i][0] = null;
+    }
+  }
+
   if (playersCaseNumber !== undefined) {
-    //unhighlightCaseNumber(caseNumber);
     unhighlightSelectedDollarValueFromSideBar(caseNumber);
     displayDollarValueOfSelectedCase(caseNumber);
+    updateGameInstructionPanel();
+
     return;
   }
 
-  // setup players case
+  setupPlayersCaseNumberAndDollarValue(caseNumber);
+}
+
+function updateGameInstructionPanel() {
+  switch (openedCases) {
+    case 2:
+      gameInstruction.innerText = "5 Cases To Open";
+      break;
+    case 3:
+      gameInstruction.innerText = "4 Cases To Open";
+      break;
+    case 4:
+      gameInstruction.innerText = "3 Cases To Open";
+      break;
+    case 5:
+      gameInstruction.innerText = "2 Cases To Open";
+      break;
+    case 6:
+      gameInstruction.innerText = "1 Case To Open";
+      break;
+    case 7:
+      gameInstruction.innerText = "Deal Or No Deal!!";
+
+      deactivateRemainingCases();
+      break;
+  }
+}
+
+function deactivateRemainingCases() {
+  for (let i = 0; i < casesAndTheirDollarValues.length + 1; i++) {
+    if (casesAndTheirDollarValues[i][0] !== null) {
+      let caseToDisableTemporarily = document.getElementById("case-" + (i + 1));
+      caseToDisableTemporarily.style.opacity = "0.4";
+      caseToDisableTemporarily.style.pointerEvents = "none";
+    }
+  }
+}
+
+function setupPlayersCaseNumberAndDollarValue(caseNumber) {
   playersCaseNumber = caseNumber;
   playersCaseDollarValue = casesAndTheirDollarValues[caseNumber][1];
 
   let playersSelectedCase = document.getElementById("playersSelectedCase");
-  playersSelectedCase.innerText = playersCaseNumber + 1; // zero index work around
+  playersSelectedCase.innerText = playersCaseNumber;
 
   let playersCaseNumberToUnhighlight = document.getElementById(
     "case-" + caseNumber
@@ -53,7 +99,7 @@ function selectedCase(caseNumber) {
   playersCaseNumberToUnhighlight.style.opacity = 0.4;
   playersCaseNumberToUnhighlight.style.pointerEvents = "none";
 
-  gameInstruction.innerText = "Select 6 Cases";
+  gameInstruction.innerText = "6 Cases To Open";
 }
 
 function unhighlightSelectedDollarValueFromSideBar(caseNumber) {
@@ -70,9 +116,12 @@ function displayDollarValueOfSelectedCase(caseNumber) {
     "case-" + caseNumber
   );
 
-  caseNumberToShowDollarValue.innerText = dollarValueOfSelectedCase;
-  caseNumberToShowDollarValue.style.fontSize = "1.9em";
+  caseNumberToShowDollarValue.innerText =
+    "$" + dollarValueOfSelectedCase.toLocaleString();
+  caseNumberToShowDollarValue.style.fontSize = "1.8em";
+  caseNumberToShowDollarValue.style.color = "#fff";
   caseNumberToShowDollarValue.style.pointerEvents = "none";
+  caseNumberToShowDollarValue.style.fontWeight = "600";
 
   if (dollarValueOfSelectedCase < 1000) {
     caseNumberToShowDollarValue.style.backgroundColor = "lightskyblue";
